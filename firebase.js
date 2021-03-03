6a2a1e7be5fb2317e84da252775e9a78477962e6
mdrewry <mdrewry0@gmail.com>
2021-03-02 21:58:07 -0500

    -created initial react project -installed mui -installed firebase -created firebase functions directory

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDdzKsxO8lZbjqUIa4U-yStKY320GMVtEs",
+  authDomain: "lolfans.firebaseapp.com",
+  projectId: "lolfans",
+  storageBucket: "lolfans.appspot.com",
+  messagingSenderId: "1090967930001",
+  appId: "1:1090967930001:web:fec790e6f27eaebf22eb30",
+  measurementId: "G-4Y8S5Y72PY",
+};
